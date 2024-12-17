ec2c0711e8eba616614d9f2cb76d4ecb229c1f60
ElkiaCode <43730058+ElkiaCode@users.noreply.github.com>
2024-12-17 20:49:25 +0100

    Update parsing.c

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -5,18 +5,20 @@ void	parsing(t_tokens *token, char *line, int tokens_size)
 	int		*size;
 	char	**line_tab;
 
-	if (check_error(line) == 1)
+
+/* pas vraiment le parsing mais le file conducteur de toute ma partie*/
+	if (check_error(line) == 1) // premier check des erreur simple
 		return ;
-	line_tab = split_command(line);
-	line_tab = ft_split(line_tab, "'<>|\"");
-	line_tab = union_tab(line_tab, 0);
-	size = tab_size(line_tab);
-	tokenizer(token, line_tab, tokens_size, size);
-	token = get_token_type(token, tokens_size);
-	token = search_for_args(token, tokens_size);
-	token = main_expand(token, tokens_size);
+	line_tab = split_command(line); // la mise sous forme de tableau de la ligne de cmd
+	line_tab = ft_split(line_tab, "'<>|\""); // les separateurs a prendre en compte individuellement
+	line_tab = union_tab(line_tab, 0); // l'union des > et < qui se suivent
+	size = tab_size(line_tab); // taille du tableau
+	tokenizer(token, line_tab, tokens_size, size); // la mise en place des tokens
+	token = get_token_type(token, tokens_size); // le typage des separateur et des cmd simple
+	token = search_for_args(token, tokens_size); // typage des arguments et des expander
+	token = main_expand(token, tokens_size); // les expanders
 	union_arg(&token, tokens_size);
-	if (final_parser(token, tokens_size) == 1)
+	if (final_parser(token, tokens_size) == 1) // parsing final et pas encore fini
 		return ;
-	print_test(token, tokens_size);
+	print_test(token, tokens_size); //le print pour le test
 }
