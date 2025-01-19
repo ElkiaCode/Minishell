61a3873f9f99489efcb1d7d7a83a4790b7a0dfe1
Isolation <parodie.clement@gmail.com>
2025-01-19 09:50:38 +0100

    probleme check sur search_for_args

--- utils/search_for_args.c
+++ utils/search_for_args.c
@@ -11,17 +11,20 @@ t_tokens	*search_for_args(t_tokens *token, int token_size)
 		j = -1;
 		while (token[i].tokens[++j])
 		{
-			if (token[i].type[j] == T_DLESS && token[i].type[j + 1] == T_ERR)
-				token[i].type[j + 1] = T_HEREDOC;
-			else if (token[i].type[j] == T_RLESS && token[i].type[j
-				+ 1] == T_ERR)
-				token[i].type[j + 1] = T_I_FILE;
-			else if (token[i].type[j] == T_RGREAT && token[i].type[j
-				+ 1] == T_ERR)
-				token[i].type[j + 1] = T_OR_FILE;
-			else if (token[i].type[j] == T_DGREAT && token[i].type[j
-				+ 1] == T_ERR)
-				token[i].type[j + 1] = T_OD_FILE;
+			if (j + 1 < token[i].token_size)
+			{
+				if (token[i].type[j] == T_DLESS && token[i].type[j + 1] == T_ERR)
+					token[i].type[j + 1] = T_HEREDOC;
+				else if (token[i].type[j] == T_RLESS && token[i].type[j
+					+ 1] == T_ERR)
+					token[i].type[j + 1] = T_I_FILE;
+				else if (token[i].type[j] == T_RGREAT && token[i].type[j
+					+ 1] == T_ERR)
+					token[i].type[j + 1] = T_OR_FILE;
+				else if (token[i].type[j] == T_DGREAT && token[i].type[j
+					+ 1] == T_ERR)
+					token[i].type[j + 1] = T_OD_FILE;
+			}
 			if (token[i].type[j] == T_D_QUOTE)
 			{
 				j++;
@@ -40,15 +43,18 @@ t_tokens	*search_for_args(t_tokens *token, int token_size)
 					j++;
 				}
 			}
-			if (token[i].type[j] == T_ERR && (token[i].type[j + 1] == T_D_QUOTE
-					|| token[i].type[j + 1] == T_S_QUOTE))
-				token[i].type[j] = T_ARG;
-			if (token[i].type[j] == T_CMD && (token[i].type[j + 1] != T_D_QUOTE
-					|| token[i].type[j + 1] != T_S_QUOTE) && token[i].type[j
-				+ 1] == T_ERR)
-				token[i].type[j + 1] = T_ARG;
-			if (token[i].type[j] == T_ARG && token[i].type[j + 1] == T_ERR)
-				token[i].type[j + 1] = T_ARG;
+			if (j + 1 < token[i].token_size)
+			{
+				if (token[i].type[j] == T_ERR && (token[i].type[j + 1] == T_D_QUOTE
+						|| token[i].type[j + 1] == T_S_QUOTE))
+					token[i].type[j] = T_ARG;
+				if (token[i].type[j] == T_CMD && (token[i].type[j + 1] != T_D_QUOTE
+						|| token[i].type[j + 1] != T_S_QUOTE) && token[i].type[j
+					+ 1] == T_ERR)
+					token[i].type[j + 1] = T_ARG;
+				if (token[i].type[j] == T_ARG && token[i].type[j + 1] == T_ERR)
+					token[i].type[j + 1] = T_ARG;
+			}
 		}
 	}
 	return (token);
