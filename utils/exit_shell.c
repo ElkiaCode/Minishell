4e8bfcc83913d37c8fa018b04986ce2544a555f5
lpolizzi <poli.loulou@gmail.com>
2025-01-21 12:23:47 +0100

    Started adding stuff to exit_shell to begin freeing memory.

--- utils/exit_shell.c
+++ utils/exit_shell.c
@@ -1,7 +1,29 @@
 #include "../includes/minishell.h"
 
+void	free_env_list(t_env *env)
+{
+	t_env	*tmp;
+
+	while (env)
+	{
+		tmp = env->next;
+		free(env->name);
+		free(env->value);
+		free(env);
+		env = tmp;
+	}
+}
+
+void free_all(t_global *data)
+{
+	if (data->env)
+		free_env_list(data->env);
+	if (data->env_tab)
+		free_tab(data->env_tab);
+}
+
 void	exit_shell(t_global *data, int exit_code)
 {
-	(void)data;
+	free_all(data);
 	exit(exit_code);
 }
