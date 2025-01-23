bdab9aadebf812f3f4a0debdb6d861bde0d7498a
ElkiaCode <43730058+ElkiaCode@users.noreply.github.com>
2025-01-23 17:07:49 +0100

    Update remove_quotes.c

--- utils/remove_quotes.c
+++ utils/remove_quotes.c
@@ -5,37 +5,49 @@ bool is_quote(char c)
     return (c == '"' || c == '\'');
 }
 
-char    *remove_quotes(char *token, int j)
+static bool handle_quote(char c, bool *in_quote, char *current_quote)
+{
+    if (*in_quote && c == *current_quote)
+    {
+        *in_quote = false;
+        *current_quote = 0;
+        return (true);
+    }
+    else if (!*in_quote && is_quote(c))
+    {
+        *in_quote = true;
+        *current_quote = c;
+        return (true);
+    }
+    return (false);
+}
+
+static int process_char(char *result, char c, bool in_quote, int j)
+{
+    if (!is_quote(c) || in_quote)
+        result[j++] = c;
+    return (j);
+}
+
+char *remove_quotes(char *token, int j)
 {
     char *result;
     int len;
     int i;
-    bool in_quote = false;
+    bool in_quote;
     char current_quote;
 
     len = ft_strlen(token);
-    current_quote = 0;
-    i = 0;
     result = malloc(len + 1);
+    if (!result)
+        return (NULL);
+    i = 0;
+    in_quote = false;
+    current_quote = 0;
     while (token[i])
     {
-        if (is_quote(token[i]))
-        {
-            if (in_quote && token[i] == current_quote)
-            {
-                in_quote = false;
-                current_quote = 0;
-            }
-            else if (!in_quote)
-            {
-                in_quote = true;
-                current_quote = token[i];
-            }
-            else
-                result[j++] = token[i];
-        }
-        else
-            result[j++] = token[i];
+        if (!handle_quote(token[i], &in_quote, &current_quote))
+            j = process_char(result, token[i], in_quote, j);
         i++;
     }
     result[j] = '\0';
