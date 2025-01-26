87b9b88ed164043b7154b423a211b8d9888ce203
LeoFalkins <poli.loulou@gmail.com>
2025-01-26 19:55:24 +0100

    Corrected input error checking but caused a segfault down the line in parsing.

--- utils/check_error.c
+++ utils/check_error.c
@@ -14,73 +14,39 @@
 
 static int	error_input(char *line)
 {
-	int	i;
+	int		i;
+	bool	s_quote;
+	bool	d_quote;
 
-	i = 0;
-	while (line && line[i])
+	i = -1;
+	s_quote = false;
+	d_quote = false;
+
+	while (line && line[++i])
 	{
-		if (line[i] && (line[i] == '&' || line[i] == ';'))
+		if (line[i] == '\'' && !d_quote)
+			s_quote = !s_quote;
+		else if (line[i] == '"' && !s_quote)
+			d_quote = !d_quote;
+		if (!s_quote && !d_quote && (line[i] == ';' || line[i] == '&'))
 			return (1);
-		if (line[i] && (line[i] == '"' || line[i] == '\''))
+		if (!s_quote && !d_quote && line[i] == '|')
 		{
-			while (line[i] && (line[i] != '"' || line[i] != '\''))
-				i++;
+			if (i == 0 || line[i + 1] == '|' || line[i + 1] == '\0')
+				return (1);
 		}
-		if (line[i])
-			i++;
 	}
+	if (s_quote || d_quote)
+		return (1);
 	return (0);
 }
 
-static int	pipe_error(char *line)
-{
-	int	i;
-
-	i = -1;
-	while (line && line[++i])
-	{
-		if (line[0] == '|')
-			return (1);
-		if (line[i] == '|' && line[i + 1] == '|')
-			return (1);
-		if (line[i] == '|' && line[i + 1] == '\0')
-			return (1);
-	}
-	return (0);
-}
-
-static int	double_quote(char *line)
-{
-	int	i;
-	int	count;
-
-	i = -1;
-	count = 0;
-	while (line && line[++i])
-		if (line[i] == '"')
-			count++;
-	return (count % 2);
-}
-
-static int	single_quote(char *line)
-{
-	int	i;
-	int	count;
-
-	i = -1;
-	count = 0;
-	while (line && line[++i])
-		if (line[i] == '\'')
-			count++;
-	return (count % 2);
-}
 
 int	check_error(char *line)
 {
 	if (!line || *line == '\0' || *line == ' ')
 		return (1);
-	if (single_quote(line) || error_input(line) || pipe_error(line)
-		|| double_quote(line) || line[ft_strlen(line) - 1] == '\\')
+	if (error_input(line) || line[ft_strlen(line) - 1] == '\\')
 	{
 		printf("error\n");
 		return (1);
